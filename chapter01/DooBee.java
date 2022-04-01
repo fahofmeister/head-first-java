8fe20f517ff2d0aae980f384744dea6b723dcd62
Fernando Hofmeister <fahofmeister@yahoo.com.br>
2022-04-01 02:31:40 -0300

    Chapter 1 examples

---
+++ chapter01/DooBee.java
@@ -0,0 +1,24 @@
+package chapter01;
+
+public class DooBee {
+
+   // Sharpen your pencil - page 13 
+
+    public static void main(String[] args) {
+
+        int x = 1;
+
+        while (x < 3){
+
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x += 1; // it's different in the book but I like this notation
+        }
+
+        if (x == 3){
+
+            System.out.println("Do");
+        }
+        
+    }
+}
