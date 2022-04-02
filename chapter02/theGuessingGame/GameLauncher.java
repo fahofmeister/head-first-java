def01532e229ef1ef92f71dc5f89645a8a9b585a
Fernando Hofmeister <fahofmeister@yahoo.com.br>
2022-04-02 09:55:35 -0300

    Chapter 2 - The guessing game

---
+++ chapter02/theGuessingGame/GameLauncher.java
@@ -0,0 +1,10 @@
+package chapter02.theGuessingGame;
+
+public class GameLauncher {
+
+    public static void main (String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+    
+}
